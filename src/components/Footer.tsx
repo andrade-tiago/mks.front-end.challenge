e2d09c43869172277208fff7eb65d413e21bf4a6
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-06-26 19:43:55 -0300

    :sparkles: feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import tw from "tailwind-styled-components";
+
+const Wrapper = tw.footer`
+  bg-gray-200
+  p-2
+`
+
+const CopyrightText = tw.p`
+  mx-auto w-max
+  text-black text-xs
+`
+
+const Footer = (): JSX.Element => {
+  return (
+    <Wrapper>
+      <CopyrightText>
+        MKS sistemas &copy; Todos os direitos reservados
+      </CopyrightText>
+    </Wrapper>
+  )
+}
+
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import ShoppingCart from "@/components/ShoppingCart"
 
 const HomePage = (): JSX.Element => {
   return (
-    <Header>
-      <ShoppingCart itemsAmount={0} />
-    </Header>
+    <>
+      <Header>
+        <ShoppingCart itemsAmount={0} />
+      </Header>
+
+      <Footer />
+    </>
   )
 }
 
