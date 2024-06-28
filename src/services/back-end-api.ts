9f13308c639bbb2a0711218e1199b815ca6aeaf9
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-06-28 19:17:29 -0300

    :label: chore: add types to env variables

--- src/services/back-end-api.ts
+++ src/services/back-end-api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backEndAPI = axios.create({
-  baseURL: import.meta.env.VITE_BACK_END_API_URL as string,
+  baseURL: import.meta.env.VITE_BACK_END_API_URL,
 })
 
 export default backEndAPI
